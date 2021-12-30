e1e4268722c40463fae733c2d9e272c7432ee949
Juan Patricio Marroquin <review0312oficial@gmail.com>
2021-12-30 04:48:50 -0500

    Usando el hook useParams

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import React from 'react';
+import { useParams } from 'react-router-dom';
 
 export default function UserPage() {
+    const params = useParams();
+    console.log(params);
+
     return (
         <div>
             User <h1>10</h1>
