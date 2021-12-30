f67a68d3453e799d7f5a0c1f40a9800eedcea55f
Juan Patricio Marroquin <review0312oficial@gmail.com>
2021-12-30 04:55:19 -0500

    Utilizando el hook useNavigate

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function DashboardPage() {
+    const navigate = useNavigate();
+    const handleClick = () => {
+        navigate('/');
+    }
+
     return (
         <div>
             <h1>Dashboard</h1>
+
+            <button onClick={handleClick}>
+                Logout
+            </button>
         </div>
     )
 }
