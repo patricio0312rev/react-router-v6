71a3182827f0fec5d281ee5edf7eb33c8bab3c8b
Juan Patricio Marroquin <review0312oficial@gmail.com>
2021-12-30 04:28:27 -0500

    Creando componente basico UsersPage

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AboutPage from './pages/AboutPage';
+import UsersPage from './pages/UsersPage';
 
 export default function App() {
     return (
@@ -9,6 +10,7 @@ export default function App() {
             <Routes>
                 <Route path='/' element={<HomePage/>} />
                 <Route path='/about' element={<AboutPage />} />
+                <Route path='/users' element={<UsersPage />} />
             </Routes>
         </BrowserRouter>
     )
