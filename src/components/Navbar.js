1983ea6e11fde92ea9990bfeae02a922e87da8e9
Juan Patricio Marroquin <review0312oficial@gmail.com>
2021-12-30 04:35:05 -0500

    Creando enlaces con etiqueta a

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,14 @@ import React from 'react'
 export default function Navbar() {
     return (
         <div>
-            Ir aqui
+            <ul>
+                <li>
+                    <a href='/about'>About</a>
+                </li>
+                <li>
+                    <a href='/users'>Users</a>
+                </li>
+            </ul>
         </div>
     )
 }
