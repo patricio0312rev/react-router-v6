9ef7d0556b63319a642c7627553be853d7fdca08
Juan Patricio Marroquin <review0312oficial@gmail.com>
2021-12-30 04:26:03 -0500

    Creando rutas básicas con componentes en folder pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import React from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AboutPage from './pages/AboutPage';
 
 export default function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={<div>Hello world</div>} />
+                <Route path='/' element={<HomePage/>} />
+                <Route path='/about' element={<AboutPage />} />
             </Routes>
         </BrowserRouter>
     )
